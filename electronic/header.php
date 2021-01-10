12c76e0a35d0fe8c6c54d33c56fa6bc6823e7c7c
Y0sia <kulagin.2001@mail.ru>
2021-01-10 23:32:20 +0500

    add 404

--- electronic/header.php
+++ electronic/header.php
@@ -16,29 +16,7 @@
 					<div class="row">
 						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 							<div class="top-menu">
-								<!-- Start Language -->
-								<ul class="language">
-									<li><a href="#"><img class="right-5" src="img/flags/gb.png" alt="#">English<i class="fa fa-caret-down left-5"></i></a>
-										<ul>
-											<li><a href="#"><img class="right-5" src="img/flags/fr.png" alt="#">French</a></li>
-											<li><a href="#"><img class="right-5" src="img/flags/gb.png" alt="#">English</a></li>
-											<li><a href="#"><img class="right-5" src="img/flags/gb.png" alt="#">English</a></li>
-										</ul>
-									</li>
-								</ul>
-								<!-- End Language -->
-								<!-- Start Currency -->
-								<ul class="currency">
-									<li><a href="#"><strong>$</strong> USD<i class="fa fa-caret-down left-5"></i></a>
-										<ul>
-											<li><a href="#">$ EUR</a></li>
-											<li><a href="#">$ GBP</a></li>
-											<li><a href="#">$ USD</a></li>
-										</ul>
-									</li>
-								</ul>
-								<!-- End Currency -->
-								<p class="welcome-msg">Default welcome msg!</p>
+								<p class="welcome-msg">Welcome!</p>
 							</div>
 							<!-- Start Top-Link -->
 						<?php	wp_nav_menu( [
