a12e5664e6d4fbf119854a7d3d47956ebb1a329c
Y0sia <kulagin.2001@mail.ru>
2021-01-12 00:38:44 +0500

    add archive-product

--- electronic/functions.php
+++ electronic/functions.php
@@ -6,6 +6,8 @@ if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/woocommerce/include/wc-simple-add-to-cart.php';
 	require get_template_directory() . '/woocommerce/include/wc-function-cart.php';
 	require get_template_directory() . '/woocommerce/include/wc-checkout-function.php';
+	require get_template_directory() . '/woocommerce/include/wc-archive-product-function.php';
+	require get_template_directory() . '/woocommerce/include/wc-function-content-product.php';
 	require get_template_directory() . '/include/woocommerce.php';
 	require get_template_directory() . '/woocommerce/include/wc-remove.php';
 }
